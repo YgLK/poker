1c6e8b96a0b78062aaaf01e5eaf5acd6c82a9039
YgLK <kuba21316@wp.pl>
2021-11-14 21:58:22 +0100

    Add comments to methods in the Server class

--- multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/Server.java
+++ multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/Server.java
@@ -1,63 +1,36 @@
 package pl.edu.agh.kis.pz1;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+
 
 public class Server {
+    // number of players connected to the server
+    protected static int numPlayers = 0;
 
-    private static int numPlayers = 0;
-    //thread safe array because while one thread is reading another
-    //might add delete some entries
-//    private static HashMap<Integer,PrintWriter> printWriters;
-    public static ArrayList<DataOutputStream> connectedServices = new ArrayList<>();
-
+    // start the server
     public static void startServer() throws IOException {
-
+        // set PORT on which the server will work
         final int PORT = 4040;
+        // open the server socket on the PORT
         ServerSocket serverSocket = new ServerSocket(PORT);
 
+        // send initialisation information about server
         System.out.println("Server started...");
         System.out.println("Wating for clients...");
 
         while (true) {
             PrintWriter tmp;
+            // accept clientSocket to the server
             Socket clientSocket = serverSocket.accept();
+            // increment count of players
             numPlayers++;
+            // create new EchoService attached to a player
             EchoService t = new EchoService(clientSocket);
-//            {
-//                public void run() {
-//                    try (
-////                            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-////                            Scanner in = new Scanner(clientSocket.getInputStream());
-//                            PrintWriter out = this.getOutPrintWriter();
-//                            Scanner in = this.getInScanner();
-//                    ){
-//                        while (in.hasNextLine()) {
-//                            String input = in.nextLine();
-//                            if (input.equalsIgnoreCase("exit")) {
-//                                numPlayers--;
-//                                break;
-//                            }
-//                            System.out.println("Number of players: " + numPlayers);
-//                            System.out.println("Received message from client: " + input);
-//                            out.println("You said:" + input);
-//                        }
-////                    }catch (IOException e) {
-////                        e.printStackTrace();
-//                    }
-//                }
-//            };
-            connectedServices.add(t.getOs());
+            // start EchoService
             t.start();
-//            for(DataOutputStream pw : connectedServices){
-//                String serverMsg = "DOes it work?";
-//                pw.writeUTF(serverMsg);
-//                pw.flush();
-//            }
         }
     }
 }
