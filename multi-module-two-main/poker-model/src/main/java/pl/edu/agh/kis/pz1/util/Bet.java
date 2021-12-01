8697c6dedce2288b150268deda9a864ecf7cb9bd
YgLK <kuba21316@wp.pl>
2021-12-01 16:01:41 +0100

    Create Bet class which handles bets

---
+++ multi-module-two-main/poker-model/src/main/java/pl/edu/agh/kis/pz1/util/Bet.java
@@ -0,0 +1,25 @@
+package pl.edu.agh.kis.pz1.util;
+
+import java.util.ArrayList;
+
+public class Bet {
+    public static int getMaxFirstBet(ArrayList<Player> players){
+        int max = 0;
+        for(Player p : players){
+            if(max < p.getfirstBid()){
+            max = p.getfirstBid();
+            }
+        }
+        return max;
+    }
+
+    public static int getMaxSecondBet(ArrayList<Player> players){
+        int max = 0;
+        for(Player p : players){
+            if(max < p.getSecondBid()){
+                max = p.getSecondBid();
+            }
+        }
+        return max;
+    }
+}
