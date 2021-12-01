1f206a4ddae8cd9813ddb060a21850de8883031c
YgLK <kuba21316@wp.pl>
2021-12-01 15:50:25 +0100

    Add exception catch to Server

--- multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/Server.java
+++ multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/Server.java
@@ -6,32 +6,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 
-public class Server {                                 // MOŻNA USTAWIĆ ŻEBY SERWER STARTOWAŁ GDY np. numPlayers = 4
-                                                        // nie trzeba bedzie sie bawic wtedy z jakims ready itd.
+public class Server {                 // MOŻNA USTAWIĆ ŻEBY SERWER STARTOWAŁ GDY np. numPlayers = 4  // nie trzeba bedzie sie bawic wtedy z jakims ready itd.
     // number of players connected to the server
     protected static int numPlayers = 0;
+    private static int gameStart = 0;
 
     // start the server
     public static void startServer() throws IOException {
         // set PORT on which the server will work
         final int PORT = 4040;
         // open the server socket on the PORT
-        ServerSocket serverSocket = new ServerSocket(PORT);
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
 
-        // send initialisation information about server
-        System.out.println("Poker game server is up and running.");
-        System.out.println("Wating for players...");
+            // send initialisation information about server
+            System.out.println("Poker game server is up and running.");
+            System.out.println("Wating for players...");
+            gameStart = 1;
 
-        while (true) {
-            PrintWriter tmp;
-            // accept clientSocket to the server
-            Socket clientSocket = serverSocket.accept();
-            // increment count of players
-            incrementNumPlayers();
-            // create new EchoService attached to a player
-            EchoService t = new EchoService(clientSocket);
-            // start EchoService
-            t.start();
+            while (true) {
+                PrintWriter tmp;
+                // accept clientSocket to the server
+                Socket clientSocket = serverSocket.accept();
+                // increment count of players
+                incrementNumPlayers();
+                // create new EchoService attached to a player
+                EchoService t = new EchoService(clientSocket);
+                // start EchoService
+                t.start();
+            }
+        } catch (IOException o){
+            o.printStackTrace();
         }
     }
 
