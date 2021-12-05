8e239f1b3ffc84d0818d8ed5a07da95428c0bc0f
YgLK <kuba21316@wp.pl>
2021-12-05 16:35:39 +0100

    Extract new method to avoid duplication of code

--- multi-module-two-main/poker-model/src/main/java/pl/edu/agh/kis/pz1/util/Gameplay.java
+++ multi-module-two-main/poker-model/src/main/java/pl/edu/agh/kis/pz1/util/Gameplay.java
@@ -4,46 +4,77 @@ package pl.edu.agh.kis.pz1.util;
 import java.util.*;
 
 
+
+/*
+gamePhases
+1 - pay Ante & deal cards
+2 - first Bid
+3 - exchange cards
+4 - second Bid
+5 - evaluate Hands and under 'leader' command there will be  winner / restart game
+*/
 public class Gameplay {
+    // phase of the game
     private static int gamePhase = 1;
+    // set containing Players who've taken cards from the table
     private static Set<Player> phase1 = new LinkedHashSet<>();
+    // map containing Players and their Bet values in the second GamePhase
     private static HashMap<Player, Integer> phase2 = new HashMap<>();
+    // set containing Players who've exchanged their cards in the third GamePhase
     private static HashSet<Player> phase3 = new HashSet<>();
+    // map containing Players and their Bet values in the fourth GamePhase
     private static HashMap<Player, Integer> phase4 = new HashMap<>();
+    // winner of the game
     private static Player winner;
+    // map containing votes from players who declared to restart the game
     private static HashMap<Player, Boolean> restartVotes = new HashMap<>();
 
 
     Gameplay(){}
 
-    /*
-    gamePhases
-    1 - pay Ante & deal cards
-    2 - first Bid
-    3 - exchange cards
-    4 - second Bid
-    5 - evaluate Hands and under 'leader' command there will be  winner / restart game
-    */
-
     // group players who have taken their cards from the table
     public static void passPhase1(Player p){
+        // add player to the set
         phase1.add(p);
+        // if all players have taken cards from the table move to the next GamePhase
         if(phase1.size() == Player.getPlayerCount()){
             incrementGamePhase();
         }
     }
 
-    // pass this phase if all bets are equal, bets != 0 and first in the
-    // queue is the same person which was at the beginning
+
+    // pass this phase if all bets are not 0 and equal and
+    // queue has accomplished full cycle
+    // (first player at the beginning of the cycle is the first player)
     public static void passPhase2(Player p){
         // put player and value of his bet to the HashSet
         phase2.put(p, p.getfirstBid());
-        ArrayList<Player> pl = new ArrayList<>(phase1);
+        checkBetStatus(p, phase2);
+    }
 
+
+    // group players who have exchanged their cards
+    public static void passPhase3(Player p){
+        phase3.add(p);
+        if(phase3.size() == Player.getPlayerCount()){
+            incrementGamePhase();
+        }
+    }
+
+
+    public static void passPhase4(Player p){
+        // put player and value of his bet to the HashSet
+        phase4.put(p, p.getSecondBid());
+        checkBetStatus(p, phase4);
+    }
+
+
+    public static void checkBetStatus(Player p, HashMap<Player, Integer> bets){
+        ArrayList<Player> pl = new ArrayList<>(phase1);
         Player lastInTheQueue = pl.get(pl.size()-1);
         // check how many unique values are there in the actual Player's bets
-        if(phase2.size() == Player.getPlayerCount()){
-            HashSet<Integer> uniqueValues = new HashSet<>(phase2.values());
+        if(bets.size() == Player.getPlayerCount()){
+            HashSet<Integer> uniqueValues = new HashSet<>(bets.values());
             // I need to check if now is the first player turn
             if(uniqueValues.size() == 1
                     // check if p equals to the last player in the order in queue
@@ -54,32 +85,6 @@ public class Gameplay {
         }
     }
 
-    // group players who have exchanged their cards
-    public static void passPhase3(Player p){
-        phase3.add(p);
-        if(phase3.size() == Player.getPlayerCount()){
-            incrementGamePhase();
-        }
-    }
-
-    public static void passPhase4(Player p){
-        // put player and value of his bet to the HashSet
-        phase4.put(p, p.getSecondBid());
-        ArrayList<Player> pl = new ArrayList<>(phase1);
-
-        Player lastInTheQueue = pl.get(pl.size()-1);
-        // check how many unique values are there in the actual Player's bets
-        if(phase4.size() == Player.getPlayerCount()){
-            HashSet<Integer> uniqueValues = new HashSet<>(phase4.values());
-            // check if now is the first player turn
-            if(uniqueValues.size() == 1
-                    // check if p equals to the last player in the order in queue
-                    // (if yes that means that all players have equal bet value, we can move further)
-                    && p == lastInTheQueue){
-                incrementGamePhase();
-            }
-        }
-    }
 
     public static int getGamePhase(){
         return gamePhase;
