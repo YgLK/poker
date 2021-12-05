c998f870f47a6040ceb248935493ae37434c5a98
YgLK <kuba21316@wp.pl>
2021-12-05 12:03:23 +0100

    Add tests for Client class

--- multi-module-two-main/poker-client/src/test/java/pl/edu/agh/kis/pz1/ClientTest.java
+++ multi-module-two-main/poker-client/src/test/java/pl/edu/agh/kis/pz1/ClientTest.java
@@ -2,6 +2,7 @@ package pl.edu.agh.kis.pz1;
 
 import static junit.framework.TestCase.assertNotNull;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -10,8 +11,8 @@ import java.io.ByteArrayInputStream;
 public class ClientTest {
 
     /**
-     * Test for the construction of Main and the
-     * main method being called
+     * Test for the construction of Client and the
+     * constructor being called
      *
      */
     @Test
@@ -21,6 +22,21 @@ public class ClientTest {
         // create Client class instance
         Client client = new Client();
         assertNotNull("Main method called on class Client.", client);
+    }
+
+    /**
+     * Test for returning String
+     * containing game instructions
+     *
+     */
+    @Test
+    public void shouldReturnInstructions() {
+        // check if method returns Strings containing specific lines of instructions
+        Assert.assertTrue(Client.getInstructions().contains("Five-card draw (Poker) INSTRUCTIONS"));
+        Assert.assertTrue(Client.getInstructions().contains("GAMEPLAN"));
+        Assert.assertTrue(Client.getInstructions().contains("USEFUL COMMANDS:"));
+        Assert.assertTrue(Client.getInstructions().contains("'stay' - inform that you don't want exchange any cards in the third phase"));
+        Assert.assertTrue(Client.getInstructions().contains("Good luck!"));
     }
 }
 
