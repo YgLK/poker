d3446a35d369c6a8488c2a6a3971f3c5d0001642
YgLK <kuba21316@wp.pl>
2021-12-06 03:53:46 +0100

    Add PlayerQueue class documentation

--- multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/PlayerQueue.java
+++ multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/PlayerQueue.java
@@ -4,21 +4,39 @@ package pl.edu.agh.kis.pz1;
 import java.util.Deque;
 import java.util.LinkedList;
 
+/**
+ * Class representing Player queue.
+ */
 public class PlayerQueue {
+    /** Queue which provides right players' turn order. */
     public static Deque<EchoService> queue = new LinkedList<>();
 
+    /**
+     * Private Constructor to avoid Class instantiation.
+     */
     private PlayerQueue(){}
 
+    /**
+     * Method which produces text representation of the current Player Queue.
+     *
+     * @return String with the actual Queue
+     */
     public static String strQueue() {
         StringBuilder strQueue = new StringBuilder("Queue: ");
+        int order = 1;
         for(EchoService e : queue){
-            strQueue.append("[").append(ClientIdentifiers.getPlayer(e).getNickname()).append("] ");
+            strQueue.append("[").append(order).append("] ").append(ClientIdentifiers.getPlayer(e).getNickname()).append(" ");
+            order++;
         }
         return String.valueOf(strQueue);
     }
-        // sometimes when its 2 players queue doesn't change order after turn
 
-    // takes first player from the queue to the end of the queue after his turn
+    /**
+     * Method which takes care of the queue order.
+     *
+     * Takes first player from the queue to the end of the queue after his turn and
+     * second player in the queue becomes first.
+     */
     public static void nextPlayer() {
         EchoService e  = PlayerQueue.queue.getFirst();
         PlayerQueue.queue.addLast(e);
