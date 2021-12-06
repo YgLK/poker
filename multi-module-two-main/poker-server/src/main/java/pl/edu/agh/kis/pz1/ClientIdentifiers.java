1c2fefa623e201f518c5289d32e3a1b26c27ebdd
YgLK <kuba21316@wp.pl>
2021-12-06 04:01:38 +0100

    Add ClientIdentifiers documentation

--- multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/ClientIdentifiers.java
+++ multi-module-two-main/poker-server/src/main/java/pl/edu/agh/kis/pz1/ClientIdentifiers.java
@@ -6,18 +6,25 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Class which helps to identify Client (EchoService) with
+ * the corresponding Player class instance.
+ */
 public class ClientIdentifiers {
-    // HashMap which contains EchoServices of the Client and attached to it instance of Player class
+    /** HashMap which contains EchoServices of the Client and attached to it instance of Player class */
     private static LinkedHashMap<EchoService, Player> players = new LinkedHashMap<>();
 
+    /**
+     * Private Constructor to avoid Class instantiation.
+     */
     private ClientIdentifiers(){};
 
-    // return players HashMap
-    public static LinkedHashMap<EchoService, Player> getPlayers(){
-        return players;
-    }
-
-    // something here doesn't work
+    /**
+     * Method which allows getting Players corresponding
+     * to each Client in the game.
+     *
+     * @return list of Players corresponding to the current Clients
+     */
     public static ArrayList<Player> getPlayersKeys(){
         ArrayList<Player> pl = new ArrayList<>();
         for(Player p : players.values()){
@@ -26,21 +33,19 @@ public class ClientIdentifiers {
         return pl;
     }
 
-    public static Player getPlayer(EchoService e){
-        return players.get(e);
-    }
-
-    // add client to the players
+    /** add client to the players */
     public static void addClient(EchoService es, Player pl) {
         ClientIdentifiers.players.put(es, pl);
     }
 
-    // remove client from the players
+    /** remove client from the players */
     public static void removeClient(EchoService es) {
         ClientIdentifiers.players.remove(es);
     }
 
-    // print information about current clients
+    /**
+     * Method used to get String with information about current clients
+     */
     public static String playersListToStr(){
         StringBuilder str = new StringBuilder("Players [" + Server.numPlayers + "]: ");
         for(Map.Entry<EchoService, Player> entry : players.entrySet()){
@@ -49,9 +54,18 @@ public class ClientIdentifiers {
         return str.toString();
     }
 
+    /** clear all players data */
     public static void clearPlayersData(){
         for(Player p : players.values()){
             p.clearPlayerData();
         }
+    }
+
+    public static LinkedHashMap<EchoService, Player> getPlayers(){
+        return players;
+    }
+
+    public static Player getPlayer(EchoService e){
+        return players.get(e);
     }
 }
