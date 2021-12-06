830bf1ef88bcf4ec21a3ebb284e7a8d8487cf4f2
YgLK <kuba21316@wp.pl>
2021-12-06 01:31:09 +0100

    Add Bet class documentation

--- multi-module-two-main/poker-model/src/main/java/pl/edu/agh/kis/pz1/util/Bet.java
+++ multi-module-two-main/poker-model/src/main/java/pl/edu/agh/kis/pz1/util/Bet.java
@@ -2,20 +2,48 @@ package pl.edu.agh.kis.pz1.util;
 
 import java.util.ArrayList;
 
+// it might be moved to the poker-common
+
+/**
+ * Class used for handling Bets.
+ * Especially when maximum placed Bet value
+ * of the current Bet phase is needed.
+ */
 public class Bet {
 
+    /**
+     * Private Constructor to avoid Class instantiation.
+     */
     private Bet(){}
 
+    /**
+     * Method used for finding the maximum value of
+     * placed bets during first Bet phase.
+     *
+     * @param players List of players
+     *                participating in the game
+     *
+     * @return  Maximum value of placed Bets during First Bet phase
+     */
     public static int getMaxFirstBet(ArrayList<Player> players){
         int max = 0;
         for(Player p : players){
-            if(max < p.getfirstBid()){
-            max = p.getfirstBid();
+            if(max < p.getFirstBid()){
+            max = p.getFirstBid();
             }
         }
         return max;
     }
 
+    /**
+     * Method used for finding the maximum value of
+     * placed bets during second Bet phase.
+     *
+     * @param players List of players
+     *                participating in the game
+     *
+     * @return  Maximum value of placed Bets during First Bet phase
+     */
     public static int getMaxSecondBet(ArrayList<Player> players){
         int max = 0;
         for(Player p : players){
